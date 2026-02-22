9f89c79c67af35efe21192c0e732e57e9606aa94
MegaBobus228 <140627917+CuboBuilder@users.noreply.github.com>
2026-02-22 13:09:10 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,12 @@ cd /home/container
 
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
-echo -e "\033[1;33mStarting with:\033[0m"
-echo -e "\033[1;32m${MODIFIED_STARTUP}\033[0m"
+echo -e "\033[1;33mStarting Nebula Yolk...\033[0m"
+echo -e "\033[1;32mRunning: ${MODIFIED_STARTUP}\033[0m"
 
 eval ${MODIFIED_STARTUP}
+
+if [ $? -ne 0 ]; then
+    echo -e "\033[1;31mStartup command failed.\033[0m"
+    exit 1
+fi
